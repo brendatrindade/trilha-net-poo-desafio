bf7eb5174eab2fec68b1c832a4c3195a26b75112
brendatrindade <brendatrindde@gmail.com>
2025-08-25 20:42:31 -0300

    feat: add testes para Nokia e Iphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+List<Smartphone> smartphones = new List<Smartphone>
+{
+	new Nokia("11999999999", "Nokia 2233", "123456789012345", 32),
+	new Iphone("11988888888", "iPhone 11", "987654321098765", 128)
+};
+
+foreach (var phone in smartphones)
+{
+	phone.ExibirInformacoes();
+	phone.Ligar();
+	phone.ReceberLigacao();
+	string app = phone is Iphone ? "Instagram" : "Whatsapp";
+	phone.InstalarAplicativo(app);
+}
